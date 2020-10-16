f393ed2366a0d04714443ed63880662a81989e80
fanqiliang <SENSETIME\fanqiliang@cn1514001447u.domain.sensetime.com>
2020-10-16 14:37:20 +0800

    init

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd `dirname $0`;
+if [ ! -d logs ];
+then
+  mkdir logs
+fi
+
 sudo docker image build -t fanqiliang/os_metricbeat:latest . &&
 sudo docker container run -itd -v $($(cd `dirname $0`);pwd)/logs/:/metricbeat/logs --name metricbeat fanqiliang/os_metricbeat /bin/bash &&
 sudo docker start metricbeat
